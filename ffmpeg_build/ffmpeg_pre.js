08df9d6b685507bb041a3b6be33b6b553cb04244
Brian Grinstead <briangrinstead@gmail.com>
2013-11-11 13:44:43 -0600

    add FFmpeg license header in generated js file

--- ffmpeg_build/ffmpeg_pre.js
+++ ffmpeg_build/ffmpeg_pre.js
@@ -1,3 +1,12 @@
+/*
+Most files in FFmpeg are under the GNU Lesser General Public License version 2.1
+or later (LGPL v2.1+). Read the file COPYING.LGPLv2.1 for details. Some other
+files have MIT/X11/BSD-style licenses. In combination the LGPL v2.1+ applies to
+FFmpeg.
+
+The source code used to build this file can be obtained at https://github.com/bgrins/videoconverter.js,
+and in zip form at https://github.com/bgrins/videoconverter.js/archive/master.zip
+*/
 
 function ffmpeg_run(opts) {
   var Module = {
