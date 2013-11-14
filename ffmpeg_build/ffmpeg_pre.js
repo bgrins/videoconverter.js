06835303677ffd064123c2e55763ee5a310f68b8
Brian Grinstead <briangrinstead@gmail.com>
2013-11-13 22:25:19 -0600

    use different pre header to allow multiple files inputted at once

--- ffmpeg_build/ffmpeg_pre.js
+++ ffmpeg_build/ffmpeg_pre.js
@@ -12,18 +12,20 @@ function ffmpeg_run(opts) {
   var Module = {
     'outputDirectory': 'output'
   };
-
   for (var i in opts) {
     Module[i] = opts[i];
   }
-
   Module['preRun'] = function() {
     FS.createFolder('/', Module['outputDirectory'], true, true);
     if (Module['fileData']) {
       FS.createDataFile('/', Module['fileName'], Module['fileData'], true, true);
     }
+    if (Module['files']) {
+      Module['files'].forEach(function(file) {
+        FS.createDataFile('/', file.name, file.data, true, true);
+      });
+    }
   };
-
   Module['postRun'] = function() {
     var handle = FS.analyzePath(Module['outputDirectory']);
     Module['return'] = handle;
