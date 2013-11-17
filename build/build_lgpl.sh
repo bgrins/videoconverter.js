f4cf2a58445b57560abea386642e00802b17914a
Brian Grinstead <briangrinstead@gmail.com>
2013-11-17 07:28:55 -0600

    set total memory at build time, since it needs to grow every time

--- build/build_lgpl.sh
+++ build/build_lgpl.sh
@@ -5,7 +5,7 @@ cd ffmpeg
 emconfigure ./configure --cc="emcc" --target-os=none --cpu=generic --arch=x86_64 --disable-ffplay --disable-ffprobe --disable-ffserver --disable-asm --enable-pic --disable-doc --disable-devices --disable-pthreads --disable-w32threads --disable-network --enable-small --disable-hwaccels --disable-parsers --disable-bsfs --disable-debug --disable-zlib
 make clean;
 emmake make;
-emcc -O2 -s VERBOSE=1 -s ASM_JS=0 -s ALLOW_MEMORY_GROWTH=1 -v libavutil/*.o libavcodec/*.o libavformat/*.o libavdevice/*.o libswresample/*.o libavfilter/*.o libswscale/*.o *.o -o ../ffmpeg.js --pre-js ../ffmpeg_pre.js --post-js ../ffmpeg_post.js
+emcc -O2 -s VERBOSE=1 -s ASM_JS=0 -s TOTAL_MEMORY=33554432 -v libavutil/*.o libavcodec/*.o libavformat/*.o libavdevice/*.o libswresample/*.o libavfilter/*.o libswscale/*.o *.o -o ../ffmpeg.js --pre-js ../ffmpeg_pre.js --post-js ../ffmpeg_post.js
 
 cd ../
 
