10986fab7b197e07377675b2fab1443224c91857
Brian Grinstead <briangrinstead@gmail.com>
2014-06-09 19:55:55 -0500

    build script updates

--- build/build_lgpl_asm.sh
+++ build/build_lgpl_asm.sh
@@ -2,14 +2,41 @@
 
 echo "Beginning Build:"
 
+rm -r dist
+mkdir -p dist
+
+cd zlib
+make clean
+emconfigure ./configure --prefix=$(pwd)/../dist --64
+emmake make
+emmake make install
+cd ..
+
 cd ffmpeg
 
-emconfigure ./configure --cc="emcc" --enable-cross-compile --target-os=none --arch=x86_32 --cpu=generic --disable-ffplay --disable-ffprobe --disable-ffserver --disable-asm --disable-doc --disable-devices --disable-pthreads --disable-w32threads --disable-network --disable-hwaccels --disable-parsers --disable-bsfs --disable-debug --disable-zlib --disable-protocols --disable-indevs --disable-outdevs --enable-protocol=file --enable-pic --enable-small
+#--enable-small
+
 make clean
+emconfigure ./configure --cc="emcc" --prefix=$(pwd)/../dist --enable-cross-compile --target-os=none --arch=x86_32 --cpu=generic \
+    --disable-ffplay --disable-ffprobe --disable-ffserver --disable-asm --disable-doc --disable-devices --disable-pthreads --disable-w32threads --disable-network \
+    --disable-hwaccels --disable-parsers --disable-bsfs --disable-debug --disable-protocols --disable-indevs --disable-outdevs --enable-protocol=file \
+
 make
-cp ffmpeg ffmpeg.bc
+make install
+
+
+cd ..
+
+cd dist
+
+rm *.bc
+
+cp lib/libz.a dist/libz.bc
+cp ../ffmpeg/ffmpeg ffmpeg.bc
+
 emcc -s OUTLINING_LIMIT=100000 -s VERBOSE=1 -s TOTAL_MEMORY=33554432 -O2 -v ffmpeg.bc -o ../ffmpeg.js --pre-js ../ffmpeg_pre.js --post-js ../ffmpeg_post.js
 
-cd ../
+cd ..
+
 
 echo "Finished Build"
